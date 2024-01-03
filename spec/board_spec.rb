2c7cd0ca032f2cc2f2ff9c3dba6f4618dad08a94
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-03 18:12:26 -0500

    Make tests for keep track, validate coordinates and placement length

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -18,16 +18,26 @@ RSpec.describe Board do
     it 'can validate coordinates' do
         board = Board.new
         expect(board.valid_coordinate?("A1")).to eq(true)
+        expect(board.valid_coordinate?("D4")).to eq(true)
+        expect(board.valid_coordinate?("A5")).to eq(false)
+        expect(board.valid_coordinate?("E1")).to eq(false)
+        expect(board.valid_coordinate?("A22")).to eq(false)
     end
 
-    xit 'can validate ship placements' do
+    it 'can validate ship placements' do
         board = Board.new
-        expect(board).to
+        cruiser = Ship.new("Cruiser", 3)
+        submarine = Ship.new("Submarine", 2)
+
+        expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+        expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+
+        
     end
 
     xit 'can render a visual representation of itself' do
         board = Board.new
-        expect(board).to
+      
     end
     
     xit 'has cell objects' do
