0f24417bc81fc86254a4ed56aa5f3fd497bf5f44
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-03 08:40:20 -0500

    Created the ship spec we test for initiallize and attributes, make test pass

---
+++ spec/ship_spec.rb
@@ -0,0 +1,18 @@
+require './lib/ship'
+
+RSpec.describe Ship do
+    it 'exits' do 
+        cruiser = Ship.new("Cruiser", 3)
+
+        expect(cruiser).to be_an_instance_of Ship
+    end 
+
+    it 'has attributes' do
+        cruiser = Ship.new("Cruiser", 3)
+
+        expect(cruiser.name).to eq("Cruiser")
+        expect(cruiser.length).to eq(3)
+        expect(cruiser.health).to eq(3)
+    end 
+
+end 
