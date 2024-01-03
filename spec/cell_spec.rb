6db8c48082d7a881ad788cb097038efde1b150d1
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-03 09:19:53 -0500

    Made the test for place a ship

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -15,4 +15,13 @@ RSpec.describe Cell do
         expect(cell.ship).to eq(nil)
         expect(cell.empty?).to eq(true)
     end
+
+    it 'can place a ship' do
+        cruiser = Ship.new("Cruiser", 3)
+        cell = Cell.new("B4")
+
+        cell.place_ship(cruiser)
+        expect(cell.ship).to eq(cruiser)
+        expect(cell.empty?).to eq(false)
+    end 
 end 
