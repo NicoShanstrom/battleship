6f383cbb9b90eb63d66bc28ceecc9a1c1094db15
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-03 08:50:17 -0500

    Make test for sunk? hit

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -15,4 +15,21 @@ RSpec.describe Ship do
         expect(cruiser.health).to eq(3)
     end 
 
+    it 'if ship is sunken' do
+        cruiser = Ship.new("Cruiser", 3)
+
+        expect(cruiser.sunk?).to eq(false)
+
+        cruiser.hit
+        expect(cruiser.health).to eq(2)
+        
+        cruiser.hit
+        expect(cruiser.health).to eq(1)
+
+        expect(cruiser.sunk?).to eq(false)
+
+        cruiser.hit
+        expect(cruiser.sunk?).to eq(true)
+    end 
+
 end 
