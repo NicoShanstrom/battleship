45133c5806011cffb1b7a1e1b2c606954bd7a932
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-03 21:30:44 -0500

    Make expect statemtents for cannot be placed diagonal and valid placement of ships

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -35,7 +35,12 @@ RSpec.describe Board do
         expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
         expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
         expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
-
+            #coordinates of the ship placement cannot be diagonal
+        expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+        expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+            #is this a valid placement?
+        expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
+        expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
     end
 
     xit 'can render a visual representation of itself' do
