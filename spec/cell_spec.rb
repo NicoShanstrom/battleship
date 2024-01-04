3c0a38b433a14804d1d5f126816a44432deef553
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-04 00:06:57 -0500

    Write test for the render actions

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -45,6 +45,22 @@ RSpec.describe Cell do
 
         cell_1.fire_upon
         expect(cell_1.render).to eq("M")
-
     end
+
+    it 'can have more render actions' do
+        cell_2 = Cell.new('C3')
+        cruiser = Ship.new('Cruiser', 3)
+
+        cell_2.place_ship(cruiser)
+        expect(cell_2.render(true)).to eq('S')
+
+        cell_2.fire_upon
+        expect(cell_2.render).to eq('H')
+        expect(cruiser.sunk?).to eq(false)
+        
+        cruiser.hit
+        cruiser.hit
+        expect(cruiser.sunk?).to eq(true)
+        expect(cell_2.render).to eq('X')
+      end
 end 
