513851f635d6904df5f678d9701c7bfa8218f26a
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-03 21:18:38 -0500

    Add additional expect statements in can validate ship placement to validate consecutive coordinates

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -28,11 +28,14 @@ RSpec.describe Board do
         board = Board.new
         cruiser = Ship.new("Cruiser", 3)
         submarine = Ship.new("Submarine", 2)
-
+            #is ship length is equal to coordinate array length
         expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
         expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+            #are the coordinates that the ship was placed on consecutive
+        expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+        expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+        expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
 
-        
     end
 
     xit 'can render a visual representation of itself' do
