bec39f67dcb8b0f146205a15a94a4f9b1486f490
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-05 10:55:56 -0500

    Create gameplay class, begin initialize and main_menu methods

---
+++ lib/gameplay.rb
@@ -0,0 +1,15 @@
+
+class Gameplay
+    
+    def initialize
+    
+    end
+
+    def main_menu
+        p "Welcome to BATTLESHIP"
+        p "Enter p to play. Enter q to quit."
+        choice = gets.chomp.downcase
+        if choice == "p"
+        
+    end
+end
