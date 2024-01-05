0b030a391364da9f602f67c29277b90bf1e7bddd
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-05 15:29:56 -0500

    Alter method and then return to original main_menu method, not finished

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -6,10 +6,18 @@ class Gameplay
     end
 
     def main_menu
-        p "Welcome to BATTLESHIP"
-        p "Enter p to play. Enter q to quit."
+       
+        puts "Welcome to BATTLESHIP"
+        puts "Enter p to play. Enter q to quit."
         choice = gets.chomp.downcase
         if choice == "p"
+            start_game
+        elsif choice == 'q'
+            exit
+        else
+            puts "Invalid choice. Please enter p or q"
+            main_menu
         end
+
     end
 end
