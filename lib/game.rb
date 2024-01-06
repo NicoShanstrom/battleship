f3cb4f14eeb114191ed97939350b9d66d70a507f
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-06 17:21:44 -0500

    Make a lot of changes to the check__coordinates method and seperated into multiple methods and made new temp_place_ship method

--- lib/game.rb
+++ lib/game.rb
@@ -1,50 +1,72 @@
 require './lib/ai'
+require './lib/board'
 class Game
+    
+    
+    def initialize
+        @ai = Ai.new
+        @board = Board.new
+        @ai.place_ships
+    end
 
-    def self.setup
-        ai = Ai.new
-        board = Board.new
-        ai.place_ships
-        
+    def setup
         puts "I have laid out my ship on the grid. 
         You now need to lay out your two ships.
         The Cruiser is three units long and the 
         Submarine is two units long."
-        puts "#{board.render_two}"
+        # require 'pry'; binding.pry
+        puts "#{@board.render_board}"
         puts "Enter the squares for the Cruiser (3 spaces):
         >" 
-        user_cruiser_coordinates = gets.chomp.split
-        user_cruiser_coordinates.each do |coordinate|
-            if !board.valid_coordinate?(coordinate)
-                exit
-            end
+        input = gets.chomp.split
+        # require 'pry'; binding.pry
+        until check_coordinates(input) == true
+            puts "Those are invalid coordinates. Please try again."
+            input = gets.chomp.split
         end
-        ship_cru = Ship.new("Cruiser", 3)
-        if board.valid_placement?(ship_cru, user_cruiser_coordinates)
-            board.place(ship_cru, user_cruiser_coordinates)
-        else 
-            exit
-        end
+        temp_place_ship(input)
 
-        puts board.render_two(true)
+        puts @board.render_board(true)
         puts "Enter the squares for the Submarine (2 spaces):
         >"
+
         user_submarine_coordinates = gets.chomp.split
         user_submarine_coordinates.each do |coordinate|
-            if !board.valid_coordinate?(coordinate)
+            if !@board.valid_coordinate?(coordinate)
                 exit
             end
         end
         ship_sub = Ship.new("Submarine", 2)
-        if board.valid_placement?(ship_sub, user_submarine_coordinates)
-            board.place(ship_sub, user_submarine_coordinates)
+        if @board.valid_placement?(ship_sub, user_submarine_coordinates)
+            @board.place(ship_sub, user_submarine_coordinates)
         else 
             exit
         end
-        puts board.render_two(true)
-        
 
-        # require 'pry' ; binding.pry
+        puts @board.render_board(true)
+        puts "Enter the squares for the Submarine (2 spaces):
+        >"
+        if user_submarine_coordinates.all? {|coordinate| @board.valid_coordinate?(coordinate)}
+            puts "Those are invalid coordinates. Please try again:"
+        end
+    end
+    
+    def check_coordinates(coordinate_array)
+        coordinate_array.each do |coordinate|
+            # require 'pry'; binding.pry
+            if @board.valid_coordinate?(coordinate)
+                return true
+            else
+                return false
+                break
+            end
+        end
+    end
 
+    def temp_place_ship(user_cruiser_coordinates)
+        ship_cru = Ship.new("Cruiser", 3)
+        if @board.valid_placement?(ship_cru, user_cruiser_coordinates)
+            @board.place(ship_cru, user_cruiser_coordinates)
+        end
     end
 end
