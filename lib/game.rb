74a9c43d22fae1c0a4f0160318d29718dce183a6
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-07 10:28:53 -0500

    Create some setup on the game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,22 +1,25 @@
 require './lib/ai'
 class Game
-    def self.setup
-        ai = Ai.new
-        board = Board.new
-        ai.place_ships
-       
-        initial_prompt
-        cruiser_user_input(board)
-        
-        puts board.render_two(true)
-        
-        submarine_user_input(board)
-        
-        puts board.render_two(true)
-        self.turn(ai, board)
+    def initialize 
+        @ai = Ai.new
+        @board = Board.new
     end
 
-    def self.turn(ai, board)
+    def setup
+        @ai.place_ships
+       
+        initial_prompt
+        cruiser_user_input(@board)
+        
+        puts @board.render_two(true)
+        
+        submarine_user_input(@board)
+        
+        puts @board.render_two(true)
+        turn(@ai, @board)
+    end
+
+    def turn(ai, board)
         puts "===============COMPUTER BOARD============="
         puts ai.board.render_two()
         puts "===============PLAYER BOARD==============="
@@ -26,14 +29,14 @@ class Game
         
     end
     
-    def self.initial_prompt
+    def initial_prompt
         puts "I have laid out my ship on the grid. 
         You now need to lay out your two ships.
         The Cruiser is three units long and the 
         Submarine is two units long."
     end 
 
-    def self.cruiser_user_input(board)
+    def cruiser_user_input(board)
         puts "#{board.render_two}"
         puts "Enter the squares for the Cruiser (3 spaces):>" 
 
@@ -43,7 +46,7 @@ class Game
         verify_input_coordinates(user_cruiser_coordinates, board, ship_cru)
     end 
 
-    def self.submarine_user_input(board)
+    def submarine_user_input(board)
         puts "Enter the squares for the Submarine (2 spaces):>"
 
         user_submarine_coordinates = gets.chomp.split
@@ -52,7 +55,7 @@ class Game
         verify_input_coordinates(user_submarine_coordinates, board, ship_sub)
     end
     
-    def self.verify_input_coordinates(user_input_coordinates, board, ship)
+    def verify_input_coordinates(user_input_coordinates, board, ship)
         if board.valid_placement?(ship, user_input_coordinates)
             board.place(ship, user_input_coordinates)
         else 
